ad84b3b67dedcba7b32a5c3b2fe98d2f60723187
frdvorak <frantisek.dvorak011@gmail.com>
2018-09-05 22:39:40 +0100

    added first custom component

---
+++ my-app/src/Elements.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Elements extends Component {
+    render(){
+        return(
+            <div className='element'>
+                <div>name: Water</div>
+                <div>color: Blue</div>
+                <div>strength: Medium</div>
+            </div>
+        )
+    }
+}
+
+export default Elements;
