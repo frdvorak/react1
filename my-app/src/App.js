6000d9cbb7b57389be93c3ea6b4d1cabf899b355
frdvorak <frantisek.dvorak011@gmail.com>
2018-09-12 21:07:53 +0100

    testing lifecycle hooks, final exercise

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -27,6 +27,13 @@ class App extends Component {
         elements : elements
       })
   }
+  componentDidMount(){
+    console.log('component mounted')
+  }
+  componentDidUpdate(prevProps, prevState){
+    console.log('component updated');
+    console.log(prevProps, prevState);
+  }
   render() {
     return (
       <div className="App">
