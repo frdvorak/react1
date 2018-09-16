68557bd7d2402aaa0879c67505e7d027384a5c37
frdvorak <frantisek.dvorak011@gmail.com>
2018-09-16 16:14:06 +0100

    delete todo implemented

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Todos from './Todos'
 
 class App extends Component {
   state = {
@@ -7,13 +8,24 @@ class App extends Component {
       {id: 2, content: 'Play Dragon Age'}
     ]
   }
+  deleteTodo = (id) => {
+    console.log(id);
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    })
+    this.setState({
+      todos: todos  //if key and value have the same name you can just type "todos" once on this line, ES6 shortening
+    })
+  }
   render() {
     return (
-      <div className="App">
-
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     );
   }
 }
+
 
 export default App;
