77a35363013cc19cb6929897a517db82944ad641
frdvorak <frantisek.dvorak011@gmail.com>
2018-09-16 18:22:56 +0100

    todo app complete

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import Todos from './Todos'
+import Todos from './Todos';
+import AddTodo from './AddTodo';
+
 
 class App extends Component {
   state = {
@@ -17,12 +19,24 @@ class App extends Component {
       todos: todos  //if key and value have the same name you can just type "todos" once on this line, ES6 shortening
     })
   }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    
+    this.setState({
+      todos: todos,
+      todo : ''
+    })
+    
+  }
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo} />
       </div>
+      
     );
   }
 }
