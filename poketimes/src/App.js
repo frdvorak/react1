5e338a7d9c46f94055df87ba2f05db0e11a67b42
frdvorak <frantisek.dvorak011@gmail.com>
2018-09-20 22:09:08 +0100

    installed react router

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
