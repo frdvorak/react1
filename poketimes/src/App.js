ed987c52833773492aa8756f99b4c358cd9a025c
frdvorak <frantisek.dvorak011@gmail.com>
2018-09-20 22:06:54 +0100

    navbar implemented

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App works
+       
+        <Navbar></Navbar>
+        <p>App works</p>
       </div>
     );
   }
