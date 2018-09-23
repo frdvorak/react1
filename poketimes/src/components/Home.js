f88c37653114932eec204193262b186d9a1f6cbe
unknown <frantisek.dvorak011@gmail.com>
2018-09-23 12:45:50 +0200

    JSONplaceholder data on home page

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
 
-const Home = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-                <p>But I've never been to the moon! Shinier than yours, meatbag. Oh, I don't have time for this. I have to go and buy a single piece of fruit with a coupon and then return it, making people wait behind me while I complain.
-
-Would you censor the Venus de Venus just because you can see her spewers? I usually try to keep my sadness pent up inside where it can fester quietly as a mental illness. Or a guy who burns down a bar for the insurance money!
-
-A sexy mistake. Now Fry, it's been a few years since medical school, so remind me. Disemboweling in your species: fatal or non-fatal? Yes! In your face, Gandhi! For one beautiful night I knew what it was like to be a grandmother. Subjugated, yet honored.
-
-It must be wonderful. Oh no! The professor will hit me! But if Zoidberg 'fixes' it… then perhaps gifts! Spare me your space age technobabble, Attila the Hun! Daddy Bender, we're hungry.</p>
-        </div>
-    )
+class Home extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res=>{
+                console.log(res);
+                this.setState({
+                    posts: res.data.slice(0,10)
+                })
+            })
+    }
+    render(){
+        const { posts } = this.state; //grabbing the posts property from the state
+        const postList = posts.length ? (
+            posts.map(post=>{
+                return (
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No posts yet</div>
+        )
+        return (
+            <div className="container">
+                <h4 className="center">Home</h4>
+                {postList}
+                </div>
+        )
+    } 
 }
 
 export default Home
--- poketimes/src/hoc/Rainbow.js
+++ poketimes/src/hoc/Rainbow.js
@@ -5,7 +5,7 @@ const Rainbow = (WrappedComponent)=> {
     const colours = ['red', 'pink', 'orange', 'blue', 'green', 'yellow'];
     const randomColour = colours[Math.floor(Math.random()*5)];
     const className = randomColour + '-text';
-    console.log(className);
+    //console.log(className);
 
     return (props) => {
         return (
