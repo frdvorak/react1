e716a4ead8788a2d64200e346d4983a234f175e8
unknown <frantisek.dvorak011@gmail.com>
2018-09-23 11:36:26 +0200

    added redirect with props.history.push

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React from 'react'
+import { NavLink, withRouter } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    setTimeout(()=>{
+        props.history.push('/about')
+    }, 2000);
     return (
         <nav className="nav-wrapper red darken-3">
             <div className='container'>
@@ -16,4 +19,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar;
+export default withRouter(Navbar);
