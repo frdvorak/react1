cec6f00264813438b1024f772d4d50ea6ac3a9e6
unknown <frantisek.dvorak011@gmail.com>
2018-09-25 21:47:03 +0200

    multiple counter components

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -4,15 +4,16 @@ class Counter extends Component {
     state = {
         count:0
     };
-    handleIncrement = () => {
+    handleIncrement = product => {
+        console.log(product);
         this.setState({ count: this.state.count +1 })
     };
-
     render() { 
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={ () =>this.handleIncrement({id: 1}) } 
+                className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
