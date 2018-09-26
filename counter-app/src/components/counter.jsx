fc4df05355f7a6d4fee016c5452d1bfbd9bff1e2
unknown <frantisek.dvorak011@gmail.com>
2018-09-26 10:29:34 +0200

    props optimised to one object

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        value: this.props.value,
+        value: this.props.counter.value,
     };
     handleIncrement = product => {
-        console.log(product);
         this.setState({ value: this.state.value +1 })
     };
     render() { 
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={ () =>this.handleIncrement({id: 1}) } 
+                <button onClick={ () =>this.handleIncrement() } 
                 className="btn btn-secondary btn-sm">Increment</button>
-                <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
     }
