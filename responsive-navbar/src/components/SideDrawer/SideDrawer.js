3d579fb38d7054c1996bf2c8fc991a966e7f62b4
unknown <frantisek.dvorak011@gmail.com>
2018-09-27 14:21:33 +0200

    navbar hamburger icon custom styles

---
+++ responsive-navbar/src/components/SideDrawer/SideDrawer.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const sideDrawer = props => (
+
+)
+
+export default sideDrawer;
--- responsive-navbar/src/components/Toolbar/Toolbar.js
+++ responsive-navbar/src/components/Toolbar/Toolbar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 import './Toolbar.css';
+import drawerToggleButton from '../SideDrawer/DrawerToggleButton';
 
 const toolbar = props => (
     <header className="toolbar">
         <nav className="toolbar_navigation">
-            <div></div>
+            <div>
+                <DrawerToggleButton/>
+            </div>
             <div className="toolbar_logo"><a href="/">THE LOGO</a></div>
             <div className="spacer"/>
             <div className="toolbar_navigation_items">
