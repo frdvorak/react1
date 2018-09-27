5373d34292fbe16e325bcc15b653243b2fcc3653
unknown <frantisek.dvorak011@gmail.com>
2018-09-27 14:07:08 +0200

    navbar custom CSS for top panel

--- responsive-navbar/src/App.js
+++ responsive-navbar/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import Toolbar from './components/Toolbar/Toolbar'
 
 class App extends Component {
--- responsive-navbar/src/components/Toolbar/Toolbar.js
+++ responsive-navbar/src/components/Toolbar/Toolbar.js
@@ -7,7 +7,8 @@ const toolbar = props => (
         <nav className="toolbar_navigation">
             <div></div>
             <div className="toolbar_logo"><a href="/">THE LOGO</a></div>
-            <div className="toolbar_navigation">
+            <div className="spacer"/>
+            <div className="toolbar_navigation_items">
                 <ul>
                     <li><a href="/">Products</a></li>
                     <li><a href="/">Users</a></li>
