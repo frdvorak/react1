0fb426f57ce50dad59321150c2b7c634df3e9789
unknown <frantisek.dvorak011@gmail.com>
2018-09-27 13:25:28 +0200

    navbar base files

---
+++ responsive-navbar/src/components/Toolbar/Toolbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './Toolbar.css';
+
+const toolbar = props => (
+    <header className="toolbar">
+        <nav className="toolbar_navigation">
+            <div></div>
+            <div className="toolbar_logo"><a href="/">THE LOGO</a></div>
+            <div className="toolbar_navigation">
+                <ul>
+                    <li><a href="/">Products</a></li>
+                    <li><a href="/">Users</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+ 
+export default toolbar;
