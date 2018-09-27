bdf18b5e3d3be3cead706befa0ef81073b6852a9
unknown <frantisek.dvorak011@gmail.com>
2018-09-27 14:42:03 +0200

    navbar side drawer

--- responsive-navbar/src/App.js
+++ responsive-navbar/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import Toolbar from './components/Toolbar/Toolbar'
+
+import Toolbar from './components/Toolbar/Toolbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{height: '100%'}}>
         <Toolbar/>
+        <SideDrawer/>
         <main style={{marginTop: '64px'}}>
           <p>This is the page content</p>
         </main>
--- responsive-navbar/src/components/SideDrawer/SideDrawer.js
+++ responsive-navbar/src/components/SideDrawer/SideDrawer.js
@@ -1,7 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import './SideDrawer.css'
 
 const sideDrawer = props => (
-
+    <nav className="side-drawer"> 
+        <ul>
+            <li><a href="/">Products</a></li>
+            <li><a href="/">Users</a></li>
+        </ul>
+    </nav>
 )
 
 export default sideDrawer;
