b1f6109efcf244fc2693162d9aa8ac5a92a3bd3f
unknown <frantisek.dvorak011@gmail.com>
2018-09-28 12:33:10 +0200

    reactportfolio router + 5 components

---
+++ portfoliosite/src/components/aboutme.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Aboutme extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Aboutme page</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Aboutme;
