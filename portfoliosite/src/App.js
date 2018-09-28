70a97a68a15a96ff494743acd3a8af1fbab3a5e9
unknown <frantisek.dvorak011@gmail.com>
2018-09-28 13:00:41 +0200

    reactportfolio all links working in both nav bars

--- portfoliosite/src/App.js
+++ portfoliosite/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Drawer, Content, Navigation } from 'react-mdl';
 import Main from './components/Main';
+import {Link} from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -10,18 +11,18 @@ class App extends Component {
       <Layout>
           <Header title="Title" scroll>
               <Navigation>
-                  <a href="/">Link</a>
-                  <a href="/">Link</a>
-                  <a href="/">Link</a>
-                  <a href="/">Link</a>
+                  <Link to="/resume">Resume</Link>
+                  <Link to="/aboutme">About Me</Link>
+                  <Link to="/projects">Projects</Link>
+                  <Link to="/contact">Contact</Link>
               </Navigation>
           </Header>
           <Drawer title="Title">
               <Navigation>
-                  <a href="/">Link</a>
-                  <a href="/">Link</a>
-                  <a href="/">Link</a>
-                  <a href="/">Link</a>
+                   <Link to="/resume">Resume</Link>
+                   <Link to="/aboutme">About Me</Link>
+                   <Link to="/projects">Projects</Link>
+                   <Link to="/contact">Contact</Link>
               </Navigation>
           </Drawer>
           <Content>
