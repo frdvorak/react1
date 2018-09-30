c22479c073261eab75c99125d59ff3b9a6acf47e
frdvorak <frantisek.dvorak011@gmail.com>
2018-09-30 16:08:43 +0200

    weather-app - if statement to prevent potential error when input empty

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -23,14 +23,16 @@ class App extends Component {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ''
-    })
+    if (city === true && country === true){
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+      })
+    }
   }
   render() {
     return (
