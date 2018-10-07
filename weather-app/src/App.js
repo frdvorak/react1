4b173e02163662657601d2730bfe95a3b29b6291
frdvorak <frantisek.dvorak011@gmail.com>
2018-10-07 23:18:49 +0100

    weather-app city tiles clickable

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,6 +6,7 @@ import Weather from './components/Weather';
 import CityTile from './components/CityTile';
 
 const API_KEY = '4f6f72308473314b6727d9fb36b07e22';
+const mesto = undefined;
 
 class App extends Component {
   state = {
@@ -16,11 +17,43 @@ class App extends Component {
     description: undefined,
     error: undefined
   }
+  tileClick = async (mesto, zeme) => {
+    console.log(mesto);
+    const city = mesto;
+    const country = zeme;
+    // call api, convert it to JSON, save that in variable 'data'
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    
+    //console.log(data.name)
+   
+      if (city && country && data.name) { //data.name is incuded so we setState only when we find city in the database
+        console.log(data);
+        this.setState({
+          city: data.name,
+          temperature: data.main.temp,
+          country: data.sys.country,
+          humidity: data.main.humidity,
+          description: data.weather[0].description,
+          error: ""
+        });
+      } else {
+        this.setState({
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "Please enter the city and country"
+        })
+
+      }
+  }
+  
   getWeather = async (e)=> {
     e.preventDefault(e);
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    
     // call api, convert it to JSON, save that in variable 'data'
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
@@ -63,16 +96,22 @@ class App extends Component {
           description={this.state.description}
           error={this.state.error}
         />
-        <div className='container row'>
-          <CityTile />
-          <CityTile />
-          <CityTile />
-          <CityTile />
-          <CityTile />
-          <CityTile />
-          <CityTile />
-          <CityTile />
-          <CityTile />
+        <div className='container row align-items-center justify-content-center'>
+          <CityTile name={'Paris'} city={'paris'} country={'fr'} tileClick={this.tileClick}/>
+          <CityTile name={'New York'} city={'new york'} country={'us'} tileClick={this.tileClick}/>
+          <CityTile name={'Berlin'} city={'berlin'} country={'de'} tileClick={this.tileClick}/>
+          <CityTile name={'Warsaw'} city={'warsaw'} country={'poland'} tileClick={this.tileClick}/>
+          <CityTile name={'Barcelona'} city={'barcelona'} country={'es'} tileClick={this.tileClick}/>
+          <CityTile name={'Florence'} city={'florence'} country={'it'} tileClick={this.tileClick}/>
+          <CityTile name={'Tokyo'} city={'tokyo'} country={'jp'} tileClick={this.tileClick}/>
+          <CityTile name={'Hong Kong'} city={'hong kong'} country={'cn'} tileClick={this.tileClick}/>
+          <CityTile name={'Kiev'} city={'kiev'} country={'ua'} tileClick={this.tileClick}/>
+          <CityTile name={'Prague'} city={'prague'} country={'cz'} tileClick={this.tileClick}/>
+          <CityTile name={'Vienna'} city={'vienna'} country={'at'} tileClick={this.tileClick}/>
+          <CityTile name={'Madrid'} city={'madrid'} country={'es'} tileClick={this.tileClick}/>
+          <CityTile name={'Venice'} city={'venice'} country={'it'} tileClick={this.tileClick}/>
+          <CityTile name={'Washington'} city={'washington'} country={'us'} tileClick={this.tileClick}/>
+          <CityTile name={'Amsterdam'} city={'amsterdam'} country={'nl'} tileClick={this.tileClick}/>
         </div>
       </div>
     );
